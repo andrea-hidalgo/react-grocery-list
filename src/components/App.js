ebde931f8ca5f51f2fde35abb6f88705701f2126
Andrea <andrea.hidalgo90@gmail.com>
2021-02-01 20:58:47 -0500

    form adds new items to grocery list

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,7 @@ import GroceryData from '../../models/GroceryData';
 import Groceries from './Groceries';
 
 export default function App(props) {
-	const [query, updateQuery] = useState({
-		url: '../../models/Groceries.js',
+	const [listItem, updateItem] = useState({
 		item: '',
 		brand: '',
 		units: '',
@@ -32,35 +31,82 @@ export default function App(props) {
 			brand: "Tony's",
 			units: '1',
 			quantity: 3,
-			isPurchased: true
+			isPurchased: false
 		}
 	]);
 
-	// useEffect(() => {
-	// 	async () => {
-	// 		try {
-	// 			const response = await fetch('../../models/Groceries.js');
-	// 			// const data = await response.json();
-	// 			await setList(response);
-	// 		} catch (error) {
-	// 			console.log(error);
-	// 		} finally {
-	// 			updateQuery({
-	// 				url: '../../models/Groceries.js',
-	// 				item: '',
-	// 				brand: '',
-	// 				units: '',
-	// 				quantity: 0,
-	// 				isPurchased: false
-	// 			});
-	// 		}
-	// 	};
-	// }, [query]);
+	const handleChange = event => {
+		updateItem({
+			...listItem,
+			...{
+				[event.target.id]: event.target.value
+			}
+		});
+	};
+
+	const handleSubmit = event => {
+		event.preventDefault();
+		setList([...groceryList, listItem]);
+		console.log(listItem);
+		console.log(groceryList);
+		updateItem({
+			item: '',
+			brand: '',
+			units: '',
+			quantity: 0,
+			isPurchased: false
+		});
+	};
 
 	return (
 		<div>
 			<h1>We Need Groceries!</h1>
 			<Groceries grocery={groceryList} />
+			<br />
+			<h2>Add an item to your list</h2>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="item">
+					Name
+					<input
+						id="item"
+						type="text"
+						value={listItem.item}
+						onChange={handleChange}
+					></input>
+				</label>
+				<br />
+				<label htmlFor="brand">
+					Brand
+					<input
+						id="brand"
+						type="text"
+						value={listItem.brand}
+						onChange={handleChange}
+					></input>
+				</label>
+				<br />
+				<label htmlFor="units">
+					Units
+					<input
+						id="units"
+						type="text"
+						value={listItem.units}
+						onChange={handleChange}
+					></input>
+				</label>
+				<br />
+				<label htmlFor="quantity">
+					Quantity
+					<input
+						id="quantity"
+						type="text"
+						value={listItem.quantity}
+						onChange={handleChange}
+					></input>
+				</label>
+				<br />
+				<input type="submit" value="Add Item"></input>
+			</form>
 		</div>
 	);
 }
--- src/components/Groceries.js
+++ src/components/Groceries.js
@@ -4,12 +4,15 @@ export default function Groceries(props) {
 	return (
 		<>
 			{props.grocery.map((product, index) => {
-				return (
+				return props.grocery[index].isPurchased === false ? (
 					<div key={props.grocery[index].item}>
 						<h1>{props.grocery[index].item}</h1>
+						<h3>brand: {props.grocery[index].brand}</h3>
 						<h3>quantity: {props.grocery[index].quantity}</h3>
 						<h3>units: {props.grocery[index].units}</h3>
 					</div>
+				) : (
+					''
 				);
 			})}
 		</>
